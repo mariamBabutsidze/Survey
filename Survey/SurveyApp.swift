feacd8e3c1d583580636c5439d497caa90ad7f60
Mariam Babutsidze <mariambabutsidze@gmail.com>
2024-03-26 15:04:56 +0400

    creates app

---
+++ Survey/SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApp.swift
+//  Survey
+//
+//  Created by Mariam Babutsidze on 26.03.24.
+//
+
+import SwiftUI
+
+@main
+struct SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
